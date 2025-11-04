0277cf1e99a5a6f26da9f6af17756d6573c11aa4
Booker Grass <bookergrass@gmail.com>
2025-11-03 20:11:41 -0600

    work 3

--- Climbing-App/src/Components/CharacterPage.jsx
+++ Climbing-App/src/Components/CharacterPage.jsx
@@ -1,32 +1,49 @@
-import React from "react"
-import Navbar from "./NavBar"
+import React, { useState } from "react";
+import Navbar from "./NavBar";
 
-function CreateBuddy(){
-    return(
-        <div>
-            <Navbar />
-            <h1>Create Your Buddy</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
-                <label for="animalDropdown">Choose an animal:</label>
-                    <select name="animalDropdown" id="animalDropdown">
-                        <option value="cat">Cat</option>
-                        <option value="dog">Dog</option>
-                        <option value="lizard">Lizard</option>
-                    </select>
-                <br/>
-                <label for="colorDropdown">Choose an color:</label>
-                    <select name="colorDropdown" id="colorDropdown">
-                        <option value="red">Red</option>
-                        <option value="black">Black</option>
-                        <option value="green">Green</option>
-                    </select>
-                <br/>
-            <img src="https://www.catbehaviourist.com/wp-content/uploads/2015/11/cat-in-tree-1.jpg"/>
-            <br/>
-            <button className = "save-buddy">Save Your Buddy</button>
-        </div>
+function CreateBuddy() {
 
-    )
+  const [selectedImage, setSelectedImage] = useState("cat");
+
+
+  const imageOptions = {
+    cat: "https://www.catbehaviourist.com/wp-content/uploads/2015/11/cat-in-tree-1.jpg",
+    dog: "https://gripped.com/wp-content/uploads/2018/03/Biscuit-Dog.jpg",
+    lizard: "https://images.pexels.com/photos/17020788/pexels-photo-17020788/free-photo-of-a-lizard-climbing-on-the-rock.jpeg",
+  };
+
+
+  const handleChange = (e) => {
+    setSelectedImage(e.target.value);
+  };
+
+  return (
+    <div>
+      <Navbar />
+
+      <h1>Create Your Buddy</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+      </p>
+
+      <label htmlFor="animalDropdown">Choose an animal:</label>
+      <select id="animalDropdown" onChange={handleChange} value={selectedImage}>
+        <option value="cat">Cat</option>
+        <option value="dog">Dog</option>
+        <option value="lizard">Lizard</option>
+      </select>
+
+      <div className="image-container">
+        <img
+          src={imageOptions[selectedImage]}
+          alt={selectedImage}
+          className="climb-image"
+        />
+      </div>
+
+      <button className="save-buddy">Save Your Buddy</button>
+    </div>
+  );
 }
 
-export default CreateBuddy
+export default CreateBuddy;
--- Climbing-App/src/Components/LandingScreen.jsx
+++ Climbing-App/src/Components/LandingScreen.jsx
@@ -1,23 +1,27 @@
-import React from "react"
-import Navbar from "./NavBar"
-import "./LandingScreen.css"
-  
-function LandingScreen(){
+import React from "react";
+import Navbar from "./NavBar";
+import "./LandingScreen.css";
+import { Link } from "react-router-dom";
 
-    return(
-<div>
-<Navbar />
-<div className= "home-image">
-  <img src="https://npr.brightspotcdn.com/dims4/default/a95eb8c/2147483647/strip/true/crop/1000x563+0+52/resize/1200x675!/quality/90/?url=http%3A%2F%2Fnpr-brightspot.s3.amazonaws.com%2Flegacy%2Fsites%2Fkwmu%2Ffiles%2F201607%2Fclimbsoill2.jpg"/>
-  <div className="centered"><h1>Welcome To Climb Buddy!</h1>  
-  <h2>Sign Up Here</h2>
-  <button className = "get-started">Get Started</button>
-  </div>
-
-</div>
-
-</div>
-);
-};
+function LandingScreen() {
+  return (
+    <div>
+      <Navbar />
+      <div className="home-image">
+        <img
+          src="https://npr.brightspotcdn.com/dims4/default/a95eb8c/2147483647/strip/true/crop/1000x563+0+52/resize/1200x675!/quality/90/?url=http%3A%2F%2Fnpr-brightspot.s3.amazonaws.com%2Flegacy%2Fsites%2Fkwmu%2Ffiles%2F201607%2Fclimbsoill2.jpg"
+          alt="Climber scaling a rock wall"
+        />
+        <div className="centered">
+          <h1>Welcome To Climb Buddy!</h1>
+          <h2>Sign Up Here</h2>
+          <Link to="/signup">
+            <button className="get-started">Get Started</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default LandingScreen;
