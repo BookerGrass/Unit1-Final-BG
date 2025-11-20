896567fa0c33daeeb889602b9cfcc406fca4f3f9
Booker Grass <bookergrass@gmail.com>
2025-11-19 19:12:53 -0600

    added content and fixed css for AboutPage

--- Climbing-App/src/Components/AboutPage.jsx
+++ Climbing-App/src/Components/AboutPage.jsx
@@ -15,18 +15,21 @@ function AboutPage() {
         <img className="about-image" src={Picture} alt="Me and My Partner" />
 
         <p className="about-text">
-          Climbing is a huge part of me and my partner's lives, but naturally,
-          like any skill, we are at different levels of experience and ability.
-          In short, I climb higher grades than her. We love climbing together,
-          but she would get discouraged when she didn't "climb as hard as she
-          should". When I saw her get down on herself, it made me think of some
-          way to fix this. I think that just walking through the doors of the
-          gym is something to be celebrated, not just climbing hard, and I want
-          to spread that feeling to every climber. Using me and my partner's
-          love for climbing and goofy little guys, I came up with this site. I
-          hope it helps others have a positive outlook on climbing.
-          <br /> Just Keep Climbing!
-          <br /> -Booker
+          <p className="about-text-first">Climbing</p> is a huge part of me and
+          my partner's lives, but naturally, like any skill, we are at different
+          levels of experience and ability. In short, I climb higher grades than
+          her. We love climbing together, but she would get discouraged when she
+          didn't "climb as hard as she should". When I saw her get down on
+          herself, it made me think of some way to fix this. I think that just
+          walking through the doors of the gym is something to be celebrated,
+          not just climbing hard, and I want to spread that feeling to every
+          climber. Using me and my partner's love for climbing and goofy little
+          guys, I came up with this site. I hope it helps others have a positive
+          outlook on climbing.
+          <br />
+          <br />
+          Just Keep Climbing!
+          <br /> -Booker + Bri
         </p>
       </div>
 
